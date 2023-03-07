ffd3f8efd73198c70818ebe8395914b030ce78ef
avmett <avmett@iu.edu>
2023-03-06 21:13:22 -0500

    missed helper

--- helper/helper.js
+++ helper/helper.js
@@ -1,14 +1,24 @@
 module.exports = {
-    getNongetNonOverlappingCount: (roomReservations,start,end)=>{
-        let reservations = roomReservations.reservations
-        let start = new Date(start),
-            end = new Date(end);
-        let count = 0;
-        for(let reservation in reservations){
+    getNonOverlappingCount: async (roomReservations,s,e)=>{
+        
+        let t1 = new Date(s),
+        t2 = new Date(e);
+        const start = new Date( t1.getTime() - t1.getTimezoneOffset() * -60000 ),
+        end = new Date( t2.getTime() - t2.getTimezoneOffset() * -60000 )
+        console.log(start+" "+end);
+        let fl = true;
+        let count = 1;
+        for(let i=await roomReservations.next(); i!=null;i=await roomReservations.next()) {
+            let reservation = i.reservationId;
+            fl=false;
             if((reservation.startDate<start && start < reservation.endDate) || (reservation.startDate<end && end < reservation.endDate)){
-                return 0;
+                break;
             }
+            count+=1;
         }
-        return 1;
+        if(fl){
+            return count-1;
+        }
+        return count;
     }
 }
